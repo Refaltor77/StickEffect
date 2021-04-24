cd145b14f961c0cfd3e8f1dd7b7085cd30c644dc
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-04-24 19:11:06 +0200

    update 3.0.0 StickEffect

--- src/StickEffect/refaltor/event/StickListener.php
+++ src/StickEffect/refaltor/event/StickListener.php
@@ -6,6 +6,7 @@ use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\item\Item;
 use pocketmine\Player;
 use StickEffect\refaltor\Stick;
 
@@ -20,11 +21,14 @@ class StickListener implements Listener
             $in = array_keys($all);
             if (in_array("{$item->getId()}:{$item->getDamage()}", $in)){
                 $stick = $all["{$item->getId()}:{$item->getDamage()}"];
+                $explode = explode(":", $stick);
                 $effect = $stick["effect"];
+                $bool = $stick["remove"];
                 if (!isset($this->cooldown[$player->getName()])) {
                     $this->cooldown[$player->getName()] = time() + $stick["cooldown"];
                     foreach ($effect as $id => $values) {
                         $player->addEffect(new EffectInstance(Effect::getEffect($id), $values["duration"], $values["niveau"], $values["visible"]));
+                        if ($bool === true) $player->getInventory()->removeItem(Item::get($stick[0], $stick[1]));
                     }
                 }elseif(time() < $this->cooldown[$player->getName()]){
                     $time = $this->cooldown[$player->getName()] - time();
