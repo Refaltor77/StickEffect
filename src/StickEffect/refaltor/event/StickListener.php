3f86d08d9b2ffb2568441a8ba158fe1da76abfae
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2021-06-08 00:47:12 +0200

    Update StickListener.php

--- src/StickEffect/refaltor/event/StickListener.php
+++ src/StickEffect/refaltor/event/StickListener.php
@@ -12,30 +12,30 @@ use StickEffect\refaltor\Stick;
 
 class StickListener implements Listener
 {
-    public $cooldown = [];
+    public array $cooldown = [];
     public function onInteract(PlayerInteractEvent $event){
         $player = $event->getPlayer();
         $item = $event->getItem();
         $all = Stick::getStick()->getConfig()->getAll();
-        if ($player instanceof Player){
-            $in = array_keys($all);
-            if (in_array("{$item->getId()}:{$item->getDamage()}", $in)){
-                $stick = $all["{$item->getId()}:{$item->getDamage()}"];
-                $effect = $stick["effect"];
-                $bool = $stick["remove"];
-                if (!isset($this->cooldown[$player->getName()])) {
-                    $this->cooldown[$player->getName()] = time() + $stick["cooldown"];
-                    foreach ($effect as $id => $values) {
-                        $player->addEffect(new EffectInstance(Effect::getEffect($id), $values["duration"], $values["niveau"], $values["visible"]));
-                        if ($bool === true) $player->getInventory()->removeItem(Item::get($stick[0], $stick[1]));
-                    }
-                }elseif(time() < $this->cooldown[$player->getName()]){
-                    $time = $this->cooldown[$player->getName()] - time();
-                    $player->sendMessage(str_replace("{time}", $time, $stick["message"]));
-                }else{
-                    unset($this->cooldown[$player->getName()]);
-                }
-            }
-        }
+        $in = array_keys($all);
+        if ($event->getAction() === PlayerInteractEvent::RIGHT_CLICK_BLOCK || $event->getAction() === PlayerInteractEvent::RIGHT_CLICK_AIR) {
+			if (in_array("{$item->getId()}:{$item->getDamage()}", $in)) {
+				$stick = $all["{$item->getId()}:{$item->getDamage()}"];
+				$effect = $stick["effect"];
+				$bool = $stick["remove"];
+				if (!isset($this->cooldown[$player->getName()])) {
+					$this->cooldown[$player->getName()] = time() + intval($stick["cooldown"]);
+					foreach ($effect as $id => $values) {
+						$player->addEffect(new EffectInstance(Effect::getEffect($id), $values["duration"], $values["niveau"], $values["visible"]));
+						if ($bool === true) $player->getInventory()->removeItem(Item::get($item->getId(), $item->getDamage()));
+					}
+				} elseif (time() < $this->cooldown[$player->getName()]) {
+					$time = $this->cooldown[$player->getName()] - time();
+					$player->sendMessage(str_replace("{time}", $time, $stick["message"]));
+				} else {
+					unset($this->cooldown[$player->getName()]);
+				}
+			}
+		}
     }
 }
